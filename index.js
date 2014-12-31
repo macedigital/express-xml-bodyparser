fdd3fc4ef4ca4746426dd34be9b7a8bf68fe968b
Matthias Adler <macedigital@gmail.com>
2014-12-31 19:59:51 +0100

    refactor: extract helper functions and reformat code
    * Doc: Add jsdoc-blocks
    * Style: Reformat code to use 2-spaces
    * Feat: Return 411 on empty post-body

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-/**
- * Connect/Express compatible middleware for parsing raw xml body
- *
- * @license MIT
+/*!
+ * express-xml-bodyparser
+ * Copyright(c) 2014 Matthias Adler
+ * MIT Licensed
  */
 
-var parseString = require('xml2js').parseString,
-    // inlined from connect's 'utils.js' file
-    utils = {
-        hasBody: function (req) {
-            var encoding = 'transfer-encoding' in req.headers,
-                length = 'content-length' in req.headers && req.headers['content-length'] !== '0';
-            return encoding || length;
-        },
-        mime: function (req) {
-            var str = req.headers['content-type'] || '';
-            return str.split(';')[0];
-        }
-    };
+/**
+ * Module dependencies.
+ */
 
-module.exports = function (opts) {
+var xmlParser = require('./lib/types/xml.js');
 
-    var options = opts || {
-        async: true,
-        explicitArray: true,
-        normalize: true,
-        normalizeTags: true,
-        trim: true
-    };
+/**
+ * Module exports.
+ */
 
-    return function xmlbodyparser(req, res, next) {
-
-        var data = '';
-
-        if (req._body) {
-            return next();
-        }
-
-        req.body = req.body || {};
-
-        if (!utils.hasBody(req) || !module.exports.regexp.test(utils.mime(req))) {
-            return next();
-        }
-
-        req._body = true;
-
-        req.setEncoding('utf-8');
-        req.on('data', function (chunk) {
-            data += chunk;
-        });
-
-        req.on('end', function () {
-            parseString(data, options, function (err, xml) {
-                if (err) {
-                    err.status = 400;
-                    return next(err);
-                }
-                req.body = xml;
-                req.rawBody = data;
-                next();
-            });
-        });
-
-    };
-
-};
-
-module.exports.regexp = /^(text\/xml|application\/([\w!#\$%&\*`\-\.\^~]+\+)?xml)$/i;
+module.exports = xmlParser;
