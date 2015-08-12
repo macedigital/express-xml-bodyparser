d737beca7051787eafcc784f88cdc829544cecf1
Matthias Adler <macedigital@gmail.com>
2015-08-12 20:03:42 +0200

    deps: update to xml2js 0.4.6+
    - Adds hacky handler for saxParser errors (even now still doesn't catch all cases of bad xml input)
    - Updates 'devDependencies' entry in `package.json` to use latest testing tools, as well as accept any version of express since v3.18
    - Adds common files and folders to `.gitignore` file

--- test/test.js
+++ test/test.js
@@ -1,222 +1,240 @@
-var assert = require('assert'),
-    xmlparser = require('./../index.js'),
-    express = require('express'),
-    request = require('supertest'),
-    itemList = {list: {item: ['item1', 'item2', 'item3']}},
-    itemsXML = '<list><item>item1</item><item>item2</item><item>item3</item></list>';
+var assert = require('assert');
+var xmlparser = require('./../index.js');
+var express = require('express');
+var request = require('supertest');
+var itemList = {
+  list: {
+    item: [
+      'item1', 'item2', 'item3'
+    ]
+  }
+};
+var itemsXML = '<list><item>item1</item><item>item2</item><item>item3</item></list>';
 
 describe('XmlParserMiddleware', function () {
 
-    describe('#testMime', function () {
+  describe('#testMime', function () {
 
-        var regexp = xmlparser.regexp;
+    var regexp = xmlparser.regexp;
 
-        it('should detect common XML mime-types', function () {
-
-            assert.equal(true, regexp.test('text/xml'));
-            assert.equal(true, regexp.test('application/xml'));
-            assert.equal(true, regexp.test('application/rss+xml'));
-            assert.equal(true, regexp.test('application/atom+xml'));
-            assert.equal(true, regexp.test('application/vnd.google-earth.kml+xml'));
-            assert.equal(true, regexp.test('application/xhtml+xml'));
-
-        });
-
-        it('should not interfere with other body parsers', function () {
-
-            assert.equal(false, regexp.test('application/json'));
-            assert.equal(false, regexp.test('application/x-www-form-urlencoded'));
-            assert.equal(false, regexp.test('multipart/form-data'));
-
-        });
-
+    it('should detect common XML mime-types', function () {
+      assert.equal(true, regexp.test('text/xml'));
+      assert.equal(true, regexp.test('application/xml'));
+      assert.equal(true, regexp.test('application/rss+xml'));
+      assert.equal(true, regexp.test('application/atom+xml'));
+      assert.equal(true, regexp.test('application/vnd.google-earth.kml+xml'));
+      assert.equal(true, regexp.test('application/xhtml+xml'));
     });
 
-    describe('#testMiddleware', function () {
-
-        var app = express();
-
-        app.use(xmlparser());
-        app.get('/', function (req, res) {
-            res.json(req.body);
-        });
-        app.post('/', function(req, res) {
-            res.json(req.body);
-        });
-      
-        it('should not run if there is no request-body', function (done) {
-          request(app)
-            .get('/')
-            .expect(200, '{}', done);
-        });
-        
-        it('should not run if there no content-type header', function (done) {
-          request(app)
-            .post('/')
-            .send(itemsXML)
-            .expect(200, '{}', done);
-        });
-      
-        it('should throw 411 on null request', function (done) {
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/xml')
-              .expect(411, done)
-            ;
-        });
-
-        it('should throw 411 on empty request body', function (done) {
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/xml')
-              .send('   ')
-              .expect(411, done)
-            ;
-        });
-
-        it('should parse xml body', function () {
-
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/vendor-spec+xml')
-              .send(itemsXML)
-              .expect(200)
-              .end(function(err, res) {
-                  if (err) throw err;
-                  assert.deepEqual(itemList, res.body);
-              });
-        });
-        
-        it('should throw 400 on invalid xml body', function (done) {
-
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/vendor-spec+xml')
-              .send('<xml>this is invalid')
-              .expect(400, done)
-            ;
-        });
-        
-    });
-  
-    describe('#testOtherBodyParser', function () {
-      
-        var app = express();
-        app.use(function fakeMiddleware(req, res, next) {
-          // simulate previous bodyparser by setting req._body = true
-          req._body = true;
-          req.body = 'fake data';
-          next();
-        });
-        app.use(xmlparser());
-        app.post('/', function(req, res) {
-          res.json(req.body);
-        });
-      
-        it('should not parse body if other bodyparser ran before', function (done) {
-          request(app)
-            .post('/')
-            .set('Content-Type', 'application/xml')
-            .send(itemsXML)
-            .expect(200, '"fake data"', done);
-        });
-      
-    });
-  
-    describe('#testCustomRegExp', function () {
-
-        // get a fresh export instead of a reference to `xmlbodyparser`
-        delete require.cache[require.resolve('../index.js')];
-
-        var middleware = require('../index.js');
-        var app = express();
-
-        middleware.regexp = /custom\/mime/i;
-
-        app.use(middleware);
-
-        app.post('/', function(req, res) {
-            res.json(req.body);
-        });
-
-        it('should permit overloading mime-type regular expression', function () {
-
-            assert.notEqual(middleware, xmlparser.regexp);
-            assert.equal(true, middleware.regexp.test('custom/mime'));
-            assert.equal(false, middleware.regexp.test('application/xml'));
-
-        });
-
-        it('should ignore non-matching content-types', function () {
-
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/xml')
-              .send(itemsXML)
-              .expect(200)
-              .end(function(err, res) {
-                  if (err) throw err;
-                  assert.deepEqual({}, res.body);
-              });
-
-        });
-
-        it('should parse matching content-types', function () {
-
-            request(app)
-              .post('/')
-              .set('Content-Type', 'custom/mime')
-              .send(itemsXML)
-              .expect(200)
-              .end(function(err, res) {
-                  if (err) throw err;
-                  assert.deepEqual(itemList, res.body);
-              });
-
-        });
-
+    it('should not interfere with other body parsers', function () {
+      assert.equal(false, regexp.test('application/json'));
+      assert.equal(false, regexp.test('application/x-www-form-urlencoded'));
+      assert.equal(false, regexp.test('multipart/form-data'));
     });
 
-    describe('#testRouteMiddleware', function () {
+  });
 
-        var app = express();
+  describe('#testMiddleware', function () {
 
-        app.post('/', function(req, res) {
-            res.json(req.body);
-        });
-        app.post('/xml', xmlparser(), function(req, res) {
-            res.json(req.body);
-        });
+    var app = express();
 
-        it('should not act as an app middleware', function () {
+    app.use(xmlparser());
 
-            request(app)
-              .post('/')
-              .set('Content-Type', 'application/xml')
-              .send(itemsXML)
-              .expect(200)
-              .end(function(err, res) {
-                  if (err) throw err;
-                  assert.deepEqual({}, res.body);
-              });
-
-        });
-
-        it('should parse route xml request', function () {
-
-            request(app)
-              .post('/xml')
-              .set('Content-Type', 'application/xml')
-              .send(itemsXML)
-              .expect(200)
-              .end(function(err, res) {
-                  if (err) throw err;
-                  assert.deepEqual(itemList, res.body);
-              });
-
-        });
-
+    app.get('/', function (req, res) {
+      res.json(req.body);
     });
+
+    app.post('/', function (req, res) {
+      res.json(req.body);
+    });
+
+    it('should not run if there is no request-body', function (done) {
+      request(app)
+        .get('/')
+        .expect(200, '{}', done);
+    });
+
+    it('should not run if there no Content-Type header', function (done) {
+      request(app)
+        .post('/')
+        .send(itemsXML)
+        .expect(200, '{}', done);
+    });
+
+    it('should not run on empty Content-Type header', function (done) {
+      request(app)
+        .post('/')
+        .set('Content-Type', '')
+        .set('Transfer-Encoding', '')
+        .expect(200, function(err, res) {
+          assert.deepEqual(res.body, {});
+          assert.deepEqual(res.rawBody, undefined);
+          done();
+        });
+    });
+
+    it('should throw 411 on fake Transfer-Encoding header', function (done) {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/xml')
+        .set('Transfer-Encoding', '')
+        .expect(411, done);
+    });
+
+    it('should throw 411 on fake Content-Length header', function (done) {
+      request(app).post('/')
+        .set('Content-Type', 'application/xml')
+        .set('Content-Length', '')
+        .expect(411, done);
+    });
+
+    it('should throw 411 on empty request body', function (done) {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/xml')
+        .send('   ')
+        .expect(411, done);
+    });
+
+    it('should parse xml body', function () {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/vendor-spec+xml')
+        .send(itemsXML)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          assert.deepEqual(itemList, res.body);
+        });
+    });
+
+    it('should throw 400 on invalid xml body', function (done) {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/vendor-spec+xml')
+        .send('<xml>this is invalid')
+        .expect(400, done);
+    });
+
+  });
+
+  describe('#testOtherBodyParser', function () {
+
+    var app = express();
+    app.use(function fakeMiddleware(req, res, next) {
+      // simulate previous bodyparser by setting req._body = true
+      req._body = true;
+      req.body = 'fake data';
+      next();
+    });
+    app.use(xmlparser());
+    app.post('/', function (req, res) {
+      res.json(req.body);
+    });
+
+    it('should not parse body if other bodyparser ran before', function (done) {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/xml')
+        .send(itemsXML)
+        .expect(200, '"fake data"', done);
+    });
+
+  });
+
+  describe('#testCustomRegExp', function () {
+
+    // get a fresh export instead of a reference to `xmlbodyparser`
+    delete require.cache[require.resolve('../index.js')];
+
+    var middleware = require('../index.js');
+    var app = express();
+
+    middleware.regexp = /custom\/mime/i;
+
+    app.use(middleware);
+
+    app.post('/', function (req, res) {
+      res.json(req.body);
+    });
+
+    it('should permit overloading mime-type regular expression', function () {
+      assert.notEqual(middleware, xmlparser.regexp);
+      assert.equal(true, middleware.regexp.test('custom/mime'));
+      assert.equal(false, middleware.regexp.test('application/xml'));
+    });
+
+    it('should ignore non-matching content-types', function () {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/xml')
+        .send(itemsXML)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          assert.deepEqual({}, res.body);
+        });
+    });
+
+    it('should parse matching content-types', function () {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'custom/mime')
+        .send(itemsXML)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          assert.deepEqual(itemList, res.body);
+        });
+    });
+
+  });
+
+  describe('#testRouteMiddleware', function () {
+    var app = express();
+
+    app.post('/', function (req, res) {
+      res.json(req.body);
+    });
+    
+    app.post('/xml', xmlparser(), function (req, res) {
+      res.json(req.body);
+    });
+
+    it('should not act as an app middleware', function () {
+      request(app)
+        .post('/')
+        .set('Content-Type', 'application/xml')
+        .send(itemsXML)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          assert.deepEqual({}, res.body);
+        });
+    });
+
+    it('should parse route xml request', function () {
+      request(app)
+        .post('/xml')
+        .set('Content-Type', 'application/xml')
+        .send(itemsXML)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          assert.deepEqual(itemList, res.body);
+        });
+    });
+
+  });
 
 });
-
